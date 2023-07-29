5ee40ee5d8738e3c7cac82dc4e4626508ccaa8b7
pmcm <pmorato187@gmail.com>
2023-07-29 18:47:18 +0800

    asdfasdfasdfasdf

--- src/components/user-profile/user-profile.tsx
+++ src/components/user-profile/user-profile.tsx
@@ -6,174 +6,163 @@ import { MiniPost } from '../mini-post/mini-post';
 import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../../context/authContext';
 import axios from 'axios';
-import { storage, upload } from '../../firebase'; // Import the Firebase storage object
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import EditIcon from '@mui/icons-material/Edit';
 import CloseIcon from '@mui/icons-material/Close';
+import { getDownloadURL, ref } from 'firebase/storage';
+import { storage } from '../../firebase';
 
 export interface UserProfileProps {
-  className?: string;
+    className?: string;
 }
 
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-user-profiles-and-templates
+ */
 export const UserProfile = ({ className }: UserProfileProps) => {
-  const [likes, setLikes] = useState<{ img: string, id: string }[]>([]);
-  const { currentUser, logout } = useContext(AuthContext)
-  const [file, setFile] = useState<File | null>(null);
-  const location = useLocation();
-  const postId = location.pathname.split("/")[2];
-  const navigate = useNavigate();
-  const [editMode, setEditMode] = useState(false);
-  const [userr, setUser] = useState<{ img: string, name: string, bio: string, username: string }>({
-    img: '',
-    name: '',
-    bio: '',
-    username: ''
-  });
-  const [userEdit, setUserEdit] = useState<{ img: string, name: string, bio: string, username: string }>({
-    img: '',
-    name: '',
-    bio: '',
-    username: ''
-  });
-  const userId = location.pathname.split("/")[2];
-  const isCurrentUser = currentUser?.id == userId;
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(`https://fbapi-668309e6ed75.herokuapp.com/api/posts/user/${postId}`);
-        setLikes(res.data);
+    const [likes, setLikes] = useState<{img: string , id: string  }[]>([]);
+    const {currentUser, logout} = useContext(AuthContext)
+    const [file, setFile] = useState<File | null>(null);
+    const location = useLocation();
+    const postId = location.pathname.split("/")[2];
+    const navigate = useNavigate();
+    const [editMode, setEditMode] = useState(false);
+    const [userr, setUser] = useState<{ img: string, name: string, bio: string, username: string}>({
+      img: '',
+      name: '',
+      bio: '',
+      username: ''
+    });
+	const [userEdit, setUserEdit] = useState<{ img: string, name: string, bio: string, username: string }>({
+		img: '',
+		name: '',
+		bio: '',
+		username: ''
+	});
+    
+	useEffect(() => {
+		const fetchData = async () => {
+		  try {
+			const res = await axios.get(`https://fbapi-668309e6ed75.herokuapp.com/api/posts/user/${postId}`);
+			setLikes(res.data);
+	
+			// Fetch user data including the profile image from Firebase Storage
+			const userRes = await axios.get(`https://fbapi-668309e6ed75.herokuapp.com/api/users/getuser/${userId}`);
+			const userData = userRes.data;
+			if (userData.img) {
+			  // Get the image URL from Firebase Storage
+			  const imgUrl = await storage.ref(userData.img).getDownloadURL();
+			  // Update the userEdit state with the image URL
+			  setUserEdit({ ...userData, img: imgUrl });
+			} else {
+			  setUserEdit(userData);
+			}
+		  } catch (err) {
+			console.log(err);
+		  }
+		};
+		fetchData();
+	  }, [postId]);
 
-        // Fetch user data including the profile image from Firebase Storage
-        const userRes = await axios.get(`https://fbapi-668309e6ed75.herokuapp.com/api/users/getuser/${userId}`);
-        const userData = userRes.data;
-        if (userData.img) {
-          // Get the image URL from Firebase Storage
-          const imgUrl = await storage.ref(userData.img).getDownloadURL();
-          // Update the userEdit state with the image URL
-          setUserEdit({ ...userData, img: imgUrl });
-        } else {
-          setUserEdit(userData);
+      useEffect(() => {
+        if (!currentUser) {
+          navigate('/login');
         }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, [postId, userId]);
+      }, [currentUser, navigate]);
 
-  useEffect(() => {
-    if (!currentUser) {
-      navigate('/login');
-    }
-  }, [currentUser, navigate]);
+      const userId = location.pathname.split("/")[2];
+      const isCurrentUser = currentUser?.id == userId;
+      const upload = async () => {
+        try {
+          const formData = new FormData();
+          if (file) {
+            formData.append("file", file);
+          }
+          const res = await axios.post("https://fbapi-668309e6ed75.herokuapp.com/api/upload", formData);
+          return res.data
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      
+      const handleClick = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+    
+        const imgUrl = await upload();
 
+		setUserEdit(userr)
+		setEditMode(false)
+    
+        try {
+          await axios.put(`https://fbapi-668309e6ed75.herokuapp.com/api/users/${userId}`, {
+            name: userr.name,
+            uname: userr.username,
+            bbio: userr.bio,
+            img: file ? imgUrl : "",
+          });
+        } catch (err) {
+          console.log(err);
+        }
+    
+        // Wait for 2 seconds before refreshing the page
+		window.location.reload()
+      };
 
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setUser((prevState) => ({
+          ...prevState,
+          [name]: value,
+        }));
+      };
 
-  useEffect(() => {
-	const fetchData = async () => {
-	  try {
-		const res = await axios.get(`https://fbapi-668309e6ed75.herokuapp.com/api/users/getuser/${userId}`);
-		const userData = res.data;
-		setUserEdit({
-		  ...userData,
-		  img: userData.img ? await storage.ref(userData.img).getDownloadURL() : '',
-		});
-	  } catch (err) {
-		console.log(err);
-	  }
-	};
-	fetchData();
-  }, [userId]);
-
-
-  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-
-    if (!file) {
-      console.log('Please select a file.');
-      return;
-    }
-
-    // Upload the image and get the URL using the 'upload' function from firebase.tsx
-    const imgUrl = await upload(file);
-
-    // Prepare the updated user data
-    const updatedUser = {
-      ...userEdit,
-      img: file ? imgUrl : "", // Use the uploaded image URL if a new file was selected
-      name: userEdit.name,
-      uname: userEdit.username,
-      bbio: userEdit.bio,
-    };
-
-    // Send the updated user data to the server
-    try {
-      await axios.put(`https://fbapi-668309e6ed75.herokuapp.com/api/users/${userId}`, updatedUser);
-    } catch (err) {
-      console.log(err);
-    }
-
-    // Wait for 2 seconds before refreshing the page
-    // ...
-
-    // Update the editMode state to return to non-edit mode
-    setEditMode(false);
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-	const { name, value } = e.target;
-	setUser((prevState) => ({
-	  ...prevState,
-	  [name]: value,
-	}));
-  };
-
-  return (
-    <>
-      <div className={classNames(styles.root, className)}>
-
-        <Navbar />
-        <div className={styles.content}>
-          <div className={styles.left1}>
-            <img
-              className={styles.profPic}
-              src={userEdit.img} // Use the Firebase Storage image URL here
-            />
-            <h1 className={styles.name}>{userEdit.name}</h1>
-            <span className={styles.userName}>@{userEdit.username}</span>
-            <span className={styles.info}>{userEdit.bio}</span>
-            {isCurrentUser && (
-              <div className={styles.editButtonShadow}>
-                <div className={styles.editButton} onClick={() => setEditMode(true)}>
-                  <p className={styles.editButtonText}>Edit Profile</p>
-                  <div className={styles.editButtonIcon}>
-                    <EditIcon sx={{ fontSize: "15px" }} />
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-          <div className={styles.right1}>
-            <div className={styles.head}>
-              <hr className={styles.divider} />
-            </div>
-            <div className={styles.bod}>
-              {likes.map((like) => (
-                <div className={classNames(styles.root, className)} key={like.id}>
-                  <Link className="link" to={`/post/${like.id}`}>
-                    <img
-                      className={styles.image}
-                      src={like.img}
-                    />
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
-      {editMode ? (
+    return (
+		<>
+			<div className={classNames(styles.root, className)}>
+				
+				<Navbar />
+				<div className={styles.content}>
+					<div className={styles.left1}>
+						<img
+							className={styles.profPic}
+							src={userEdit.img}
+						/>
+						<h1 className={styles.name}>{userEdit.name}</h1>
+						<span className={styles.userName}>@{userEdit.username}</span>
+						<span className={styles.info}>{userEdit.bio}</span>
+						{isCurrentUser && (
+							<div className={styles.editButtonShadow}>
+								<div className={styles.editButton} onClick={() => setEditMode(true)} >
+									<p className={styles.editButtonText}>Edit Profile</p>
+									<div className={styles.editButtonIcon}>
+										<EditIcon sx={{fontSize: "15px"}}/>
+									</div>
+								</div>
+							</div>
+						)}
+					</div>
+					<div className={styles.right1}>
+						<div className={styles.head}>
+							<hr className={styles.divider} />
+						</div>
+						<div className={styles.bod}>
+							{likes.map((like) => (
+								<div className={classNames(styles.root, className)} >
+									<Link className="link" to={`/post/${like.id}`}>
+										<img
+											className={styles.image}
+											src={like.img}
+										/>
+									</Link>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+				<Footer />
+			</div>
+			{editMode ? (
 				// Edit mode UI
 				<>
 					<div className={styles.editProfileOverlay}></div>
@@ -210,17 +199,10 @@ export const UserProfile = ({ className }: UserProfileProps) => {
 							</div>
 							<div className={styles.editProfileDividerThin}></div>
 							<div className={styles.editProfileFormBio}>
-								<p className={styles.editProfileFormBioLabel}>Bio</p>
+								<p className={styles.editProfileFormBioLabel}>User Name</p>
 								<div className={styles.editProfileFormBioRight}>
-								<textarea
-								className={styles.editProfileBioTextArea}
-								name="bio"
-								value={userr.bio || ""} // Add a conditional check to handle the case when userr.bio is null or undefined
-								onChange={handleChange}
-								/>
-								<p className={styles.editProfileFormBioText}>
-								{(userr.bio ? userr.bio.length : 0)}/60
-								</p>
+									<textarea className={styles.editProfileBioTextArea} name="bio" value={userr.bio} onChange={handleChange} />
+									<p className={styles.editProfileFormBioText}>{userr.bio.length}/60</p>
 								</div>
 							</div>
 						</div>
@@ -229,6 +211,6 @@ export const UserProfile = ({ className }: UserProfileProps) => {
 					</div>
 				</>
 			) : <></>}
-    </>
-  );
+		</>
+	);
 };
